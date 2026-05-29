4cb408b1bfcd7c59f9074f6c7b1ab15f700bd754
Johan <obiyohan@free.fr>
2026-05-29 11:56:07 +0200

    fix data prep script

--- src/data_prep_feat.py
+++ src/data_prep_feat.py
@@ -16,7 +16,6 @@ warnings.filterwarnings('ignore')
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-
 # Get the directory of the current script
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -24,8 +23,8 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 data_train_path = os.path.join(script_dir, '../data/init/application_train.csv')
 data_test_path = os.path.join(script_dir, '../data/init/application_test.csv')
 # Path to saved data
-data_train_saved_path = os.path.join(script_dir, '../data/application_train_processed.csv')
-data_test_saved_path = os.path.join(script_dir, '../data/application_test_processed.csv')
+data_train_saved_path = os.path.join(script_dir, '../data/application_train_processed_features.csv')
+data_test_saved_path = os.path.join(script_dir, '../data/application_test_processed_features.csv')
 
 # Training data
 app_train = pd.read_csv(data_train_path)
@@ -37,14 +36,12 @@ app_test = pd.read_csv(data_test_path)
 print('Testing data shape: ', app_test.shape)
 app_test.head()
 
-
 # Check categorical columns
 print("Colonnes catégorielles:")
 for col in app_train.select_dtypes('object').columns:
     n_unique = app_train[col].nunique()
     n_missing = app_train[col].isna().sum()
     print(f"{col}: {n_unique} classes uniques, {n_missing} valeurs manquantes")
-
 
 # Create a label encoder object
 le = LabelEncoder()
@@ -69,10 +66,8 @@ for col in app_train.select_dtypes('object').columns:
             
 print('%d columns were label encoded.' % le_count)
 
-
 print('Training Features shape: ', app_train.shape)
 print('Testing Features shape: ', app_test.shape)
-
 
 # one-hot encoding of categorical variables
 app_train = pd.get_dummies(app_train)
@@ -80,7 +75,6 @@ app_test = pd.get_dummies(app_test)
 
 print('Training Features shape: ', app_train.shape)
 print('Testing Features shape: ', app_test.shape)
-
 
 train_labels = app_train['TARGET']
 
@@ -93,19 +87,16 @@ app_train['TARGET'] = train_labels
 print('Training Features shape: ', app_train.shape)
 print('Testing Features shape: ', app_test.shape)
 
-
 # Create an anomalous flag column
 app_train['DAYS_EMPLOYED_ANOM'] = app_train["DAYS_EMPLOYED"] == 365243
 
 # Replace the anomalous values with nan
 app_train['DAYS_EMPLOYED'].replace({365243: np.nan}, inplace = True)
 
-
 app_test['DAYS_EMPLOYED_ANOM'] = app_test["DAYS_EMPLOYED"] == 365243
 app_test["DAYS_EMPLOYED"].replace({365243: np.nan}, inplace = True)
 
 print('There are %d anomalies in the test data out of %d entries' % (app_test["DAYS_EMPLOYED_ANOM"].sum(), len(app_test)))
-
 
 # Age information into a separate dataframe
 age_data = app_train[['TARGET', 'DAYS_BIRTH']]
@@ -115,24 +106,85 @@ age_data['YEARS_BIRTH'] = age_data['DAYS_BIRTH'] / 365
 age_data['YEARS_BINNED'] = pd.cut(age_data['YEARS_BIRTH'], bins = np.linspace(20, 70, num = 11))
 age_data.head(10)
 
-
 # Group by the bin and calculate averages
 age_groups  = age_data.groupby('YEARS_BINNED').mean()
 age_groups
 
+# Feature Engineering - Polynomial Features
 
-print('Training Features shape: ', app_train.shape)
-print('Testing Features shape: ', app_test.shape)
+# Make a new dataframe for polynomial features
+poly_features = app_train[['EXT_SOURCE_1', 'EXT_SOURCE_2', 'EXT_SOURCE_3', 'DAYS_BIRTH', 'TARGET']]
+poly_features_test = app_test[['EXT_SOURCE_1', 'EXT_SOURCE_2', 'EXT_SOURCE_3', 'DAYS_BIRTH']]
 
+# imputer for handling missing values
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(strategy = 'median')
+
+poly_target = poly_features['TARGET']
+
+poly_features = poly_features.drop(columns = ['TARGET'])
+
+# Need to impute missing values
+poly_features = imputer.fit_transform(poly_features)
+poly_features_test = imputer.transform(poly_features_test)
+
+from sklearn.preprocessing import PolynomialFeatures
+                                  
+# Create the polynomial object with specified degree
+poly_transformer = PolynomialFeatures(degree = 3)
+
+# Train the polynomial features
+poly_transformer.fit(poly_features)
+
+# Transform the features
+poly_features = poly_transformer.transform(poly_features)
+poly_features_test = poly_transformer.transform(poly_features_test)
+print('Polynomial Features shape: ', poly_features.shape)
+
+poly_transformer.get_feature_names_out(input_features = ['EXT_SOURCE_1', 'EXT_SOURCE_2', 'EXT_SOURCE_3', 'DAYS_BIRTH'])[:15]
+
+# Create a dataframe of the features 
+poly_features = pd.DataFrame(poly_features, 
+                             columns = poly_transformer.get_feature_names_out(['EXT_SOURCE_1', 'EXT_SOURCE_2', 
+                                                                           'EXT_SOURCE_3', 'DAYS_BIRTH']))
+
+# Add in the target
+poly_features['TARGET'] = poly_target
+
+# Find the correlations with the target
+poly_corrs = poly_features.corr()['TARGET'].sort_values()
+
+# Display most negative and most positive
+print(poly_corrs.head(10))
+print(poly_corrs.tail(5))
+
+# Put test features into dataframe
+poly_features_test = pd.DataFrame(poly_features_test, 
+                                  columns = poly_transformer.get_feature_names_out(['EXT_SOURCE_1', 'EXT_SOURCE_2', 
+                                                                                'EXT_SOURCE_3', 'DAYS_BIRTH']))
+
+# Merge polynomial features into training dataframe
+poly_features['SK_ID_CURR'] = app_train['SK_ID_CURR']
+app_train_poly = app_train.merge(poly_features, on = 'SK_ID_CURR', how = 'left')
+
+# Merge polnomial features into testing dataframe
+poly_features_test['SK_ID_CURR'] = app_test['SK_ID_CURR']
+app_test_poly = app_test.merge(poly_features_test, on = 'SK_ID_CURR', how = 'left')
+
+# Align the dataframes
+app_train_poly, app_test_poly = app_train_poly.align(app_test_poly, join = 'inner', axis = 1)
+
+# Print out the new shapes
+print('Training data with polynomial features shape: ', app_train_poly.shape)
+print('Testing data with polynomial features shape:  ', app_test_poly.shape)
 
 # Drop SK_ID_CURR columns
-app_train.drop('SK_ID_CURR', axis = 1, inplace = True)
-app_test.drop('SK_ID_CURR', axis = 1, inplace = True)
+app_train_poly.drop('SK_ID_CURR', axis = 1, inplace = True)
+app_test_poly.drop('SK_ID_CURR', axis = 1, inplace = True)
 
-print('Training Features shape: ', app_train.shape)
-print('Testing Features shape: ', app_test.shape)
-
+print('Training Features shape: ', app_train_poly.shape)
+print('Testing Features shape: ', app_test_poly.shape)
 
 # Save the processed data in csv files
-app_train.to_csv(data_train_saved_path, index=False)
-app_test.to_csv(data_test_saved_path, index=False)
+app_train_poly.to_csv(data_train_saved_path, index=False)
+app_test_poly.to_csv(data_test_saved_path, index=False)
